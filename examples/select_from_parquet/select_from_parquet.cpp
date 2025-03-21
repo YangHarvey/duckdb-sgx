07cf03df0c2644bb917714a9c5da38f352f7d84a
YangHarvey <2998647748@qq.com>
2025-03-21 09:39:30 +0000

    add select from parquet example

---
+++ examples/select_from_parquet/select_from_parquet.cpp
@@ -0,0 +1,31 @@
+#include "duckdb.hpp"
+
+using namespace duckdb;
+
+int main() {
+	DuckDB db(nullptr);
+
+    Connection con(db);
+
+    // 启用 Profiler
+    con.EnableProfiling();
+
+    auto result = con.Query(R"(
+        SELECT l_returnflag, l_linestatus,
+            sum(l_quantity) AS sum_qty,
+            sum(l_extendedprice) AS sum_base_price,
+            sum(l_extendedprice * (1 - l_discount)) AS sum_disc_price,
+            sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) AS sum_charge,
+            avg(l_quantity) AS avg_qty,
+            avg(l_extendedprice) AS avg_price,
+            avg(l_discount) AS avg_disc,
+            count(*) AS count_order
+        FROM read_parquet(lineitem.parquet)
+        WHERE l_shipdate <= CAST('1998-09-02' AS date)
+        GROUP BY l_returnflag, l_linestatus
+        ORDER BY l_returnflag, l_linestatus
+    )");
+	result->Print();
+
+    return 0;
+}
